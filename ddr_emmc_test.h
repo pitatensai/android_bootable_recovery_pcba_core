0f78beda342d6e72d4d474220b591bffe043c700
Bian Jin chen <kenjc.bian@rock-chips.com>
2020-03-20 10:37:35 +0800

    Initial PCBA for Rockchip based on AOSP recovery.
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>
    Change-Id: Id4eae3ec7f32c98cfdd817effb24c356918ebb37

---
+++ ddr_emmc_test.h
@@ -0,0 +1,9 @@
+#ifndef __DDR_EMMC_TEST_H_
+#define __DDR_EMMC_TEST_H_
+
+char *flash_test();
+char *ddr_test();
+
+
+
+#endif
---
+++ res/mmctester.sh
@@ -0,0 +1,47 @@
+#!/system/bin/sh
+
+#source send_cmd_pipe.sh
+
+nr="0"
+mmcblk="/dev/block/mmcblk$nr"
+mmcp=$mmcblk
+
+while true; do
+    while true; do
+        if [ -b "$mmcblk" ]; then
+            busybox  sleep 1
+            if [ -b "$mmcblk" ]; then
+                echo "card$nr insert"
+                break
+            fi
+        else
+            busybox sleep 1
+        fi
+    done
+
+    if [ ! -d "/tmp/extsd" ]; then
+        busybox mkdir -p /tmp/extsd
+    fi
+
+    mmcp=$mmcblk
+    mount -t vfat $mmcp /tmp/extsd
+    if [ $? -ne 0 ]; then
+        mmcp=$mmcblk"p1"
+        mount -t vfat $mmcp /tmp/extsd
+        if [ $? -ne 0 ]; then
+            exit 0
+            busybox sleep 3
+            continue 2
+        fi
+    fi
+    break
+done
+
+capacity=`df | grep "/tmp/extsd" | busybox awk '{printf $2}'`
+echo "$mmcp: $capacity"
+
+umount /tmp/extsd
+
+echo $capacity > /data/sd_capacity
+
+exit 1
